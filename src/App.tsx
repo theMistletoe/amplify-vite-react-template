a6150e1cae12caee0149f5b9219c513348616ce9
theMistletoe <katachiwotodomete@gmail.com>
2024-06-14 19:20:59 +0900

    feat: integrate AWS Amplify UI React components for authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
@@ -24,6 +27,9 @@ function App() {
   }
 
   return (
+        
+    <Authenticator>
+      {({ signOut }) => (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
@@ -39,7 +45,11 @@ function App() {
           Review next step of this tutorial.
         </a>
       </div>
+      <button onClick={signOut}>Sign out</button>
     </main>
+        
+      )}
+      </Authenticator>
   );
 }
 
